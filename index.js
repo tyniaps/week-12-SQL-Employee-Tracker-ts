e4e5e07caaf776354692acbe9a6ecf7fca915ec0
KnightNimbus <ty.s.spain@gmail.com>
2024-03-24 22:17:43 +0000

    opened function for main menu questions and console logs to prompt user

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const inquirer = require ('inquirer'); 
 const mySql2 = require ('mysql2/promise');
+
+function = mainMenu() {
+console.log("Welcome to the SQL Employee Tracker");
+console.log("What would you like to do?");
+
+
+
+
+
+
+}
