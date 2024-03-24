fe4a5a280237e55223d9dc39f539db7dcddf9687
KnightNimbus <ty.s.spain@gmail.com>
2024-03-24 22:30:56 +0000

    prompts/questions added for main menu

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const inquirer = require ('inquirer'); 
 const mySql2 = require ('mysql2/promise');
 
-function = mainMenu() {
-console.log("Welcome to the SQL Employee Tracker");
-console.log("What would you like to do?");
+function mainMenu() {
+    
+    console.log("Welcome to the SQL Employee Tracker");
+    console.log("What would you like to do?");
 
+        inquirer.prompt(
+    [{
+        type: "list",
+        name: "menuOptions",
+        message: "Please choose from the options below:",
+        choices: [
+            "View Departments",
+            "Add New Department",
+            "View Job Roles",
+            "Add New Job Role",
+            "View Employees",
+            "Add New Employee",
+            "Exit",
+        ], 
+        
+        default: "View Departments",
+    }]
+    )
 
-
-
-
+    
 
 }
