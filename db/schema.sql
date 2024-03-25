f52ea8fb19793aef0c1ced874564a5c615bcc161
KnightNimbus <ty.s.spain@gmail.com>
2024-03-25 01:35:07 +0000

    update to schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -28,6 +28,8 @@ CREATE TABLE employee (
     first_name VARCHAR (30) NOT NULL,
     last_name VARCHAR (30) NOT NULL,
     role_id INTEGER,
+    department_id INTEGER,
+    manager_id INTEGER,
     PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES role(id),
     ON DELETE SET NULL
